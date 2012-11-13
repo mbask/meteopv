4993837f647f53196090a292fb37815e85473a2f
Marco Bascietto <marco@bascietto.name>
2012-11-13 02:42:52 +0100

    Added colnames to matrix

--- R/getSunCoordinates.R
+++ R/getSunCoordinates.R
@@ -4,10 +4,12 @@
 #' 
 #' @param time a POSIXct class object
 #' @param locCoordinates a vector holding longitude and latitude of a single location on earth
-#' @return a matrix of azimuth and height
+#' @return a matrix of 2 columns, "\code{azimuth}" and "\code{height}" of sun
 #' @import maptools
 #' @export
 #' @author Marco Bascietto \email{marco@@bascietto.name}
 getSunCoordinates <- function(locCoordinates, time) {
-  maptools::solarpos(locCoordinates, time)
+  tmp <- maptools::solarpos(locCoordinates, time)
+  colnames(tmp) <- c("azimuth", "height")
+  return(tmp)
 }
