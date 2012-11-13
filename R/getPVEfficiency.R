a41d483fa04700100634c890b7fee530446deeaa
Marco Bascietto <marco@bascietto.name>
2012-11-13 08:44:58 +0100

    Updated documentation

--- R/getPVEfficiency.R
+++ R/getPVEfficiency.R
@@ -18,13 +18,14 @@
 #' As for the first set \code{cfg} must hold:
 #' \itemize{
 #'   \item \code{NOCT} Nominal Operating Cell Temperature in \eqn{^\circ}C
-#'   \item \code{effCondStand_eta} \eqn{\gamma_{std}} in \eqn{\%}
-#'   \item \code{coeffPotenza_gamma} \eqn{\gamma} in \eqn{\%}
+#'   \item \code{etaStd} \eqn{\gamma_{std}} in \eqn{\%}
+#'   \item \code{gamma} \eqn{\gamma} in \eqn{\%}
 #' }
 #' As for the second set \code{cfg} must hold:
 #' \itemize{
 #'   \item \code{geoCoord} a matrix of two columns: longitude and latitude for each location
-#'   \item \code{angoloPannelli} the tilt angle of PV cells (\eqn{0^\circ} horizontal)
+#'   \item \code{tilt} the tilt angle of PV cells (\eqn{0^\circ} for an horizontal plane, \eqn{90^\circ} for a vertical plane)
+#'   \item \code{PVlosses} PV system losses in \eqn{[0..1]}
 #' }
 #' 
 #' @references F. Roveda, \emph{L'effetto della temperatura sulla efficienza dei moduli fotovoltaici: cosa sapere sul NOCT}, 2011, \url{http://www.energyhunters.it}
@@ -33,7 +34,7 @@
 #' @references W. Maranda and M. Piotrowicz, \emph{Extraction of thermal model parameters for field-installed photovoltaic module}, 2010, 27th International Conference on Microelectronics Proceedings (MIEL) (presented at the 2010 27th International Conference on Microelectronics Proceedings (MIEL), IEEE, 2010), 153-156.
 #'
 #' @param env.town a \code{data.frame} holding a \code{time}, \code{place}, \code{tempEsterna} and \code{irraggiamento}
-#' @param cfg a \code{list} holding several cfg infos (to be expanded...)
+#' @param cfg a \code{list} holding several cfg infos, see Details
 #' @export
 #' @author Marco Bascietto \email{marco@@bascietto.name}
 getPVEfficiency <- function(env.town, cfg) {
@@ -42,7 +43,7 @@ getPVEfficiency <- function(env.town, cfg) {
   
   env.town <- within(env.town, {
     # Correzione per l'inclinazione dei pannelli (eq. derivata a naso da me, va verificata)
-    inclCorrection      <- sin((tmp.sun[, "height"] + cfg$angoloPannelli) * pi / 180)
+    inclCorrection      <- sin((tmp.sun[, "height"] + cfg$tilt) * pi / 180)
     # W/m^2 irraggiamento corretto per l'inclinazione dei pannelli
     #irraggiamentoIncl   <- irraggiamento * inclCorrection
     # Temperatura pannello, eq. citata in "L'effetto della temperatura sull’efficienza dei moduli fotovoltaici: cosa sapere sul NOCT"
@@ -50,15 +51,15 @@ getPVEfficiency <- function(env.town, cfg) {
     # Temperatura pannello inclinato 
     #tempPannelloIncl <- tempEsterna + ((cfg$NOCT - 20) / 800) * irraggiamentoIncl
     # Efficienza, eq. citata in "L'effetto della temperatura sull’efficienza dei moduli fotovoltaici: cosa sapere sul NOCT"
-    efficienza      <- (cfg$effCondStand_eta / 100 * (1 - cfg$coeffPotenza_gamma / 100 * (tempPannello - 25)))
+    efficienza      <- (cfg$etaStd / 100 * (1 - cfg$gamma / 100 * (tempPannello - 25)))
     # Efficienza, pannello inclinato
-    #efficienzaIncl  <- (cfg$effCondStand_eta / 100 * (1 - cfg$coeffPotenza_gamma / 100 * (tempPannelloIncl - 25)))
+    #efficienzaIncl  <- (cfg$etaStd / 100 * (1 - cfg$gamma / 100 * (tempPannelloIncl - 25)))
     # W/m^2 Potenza elettrica teorica
     potElettrica         <- irraggiamento * efficienza
     # W/m^2 Potenza elettrica corretta per l'inclinazione dei pannelli
     #potElettricaIncl     <- potElettrica * inclCorrection
     # W/m^2 Potenza elettrica corretta per l'inclinazione dei pannelli e per le perdite di carico
-    #potElettricaInclPerd <- potElettricaIncl / (1 + cfg$perditeSistemaFV)
+    #potElettricaInclPerd <- potElettricaIncl / (1 + cfg$PVlosses)
     # W/m^2 Potenza termica teorica, assumendo che tutto ciò che non si trasforma in energia elettrica si trasforma in energia termica
     # QUESTA `E LA EQUAZIONE CRITICA DELLA CONVERSIONE IN CALORE DELL'ENERGIA DI SCARTO
     potTermica     <- irraggiamento * (1 - efficienza)
