9d277ee57119d229d288ba1ee775ec24bf830a11
Marco Bascietto <marco@bascietto.name>
2012-11-13 02:23:57 +0100

    Updated docs

--- R/getSunCoordinates.R
+++ R/getSunCoordinates.R
@@ -1,5 +1,7 @@
 #' Estimates Sun azimuth and height above a location
 #' 
+#' It is just a wrapper around maptools::solarpos
+#' 
 #' @param time a POSIXct class object
 #' @param locCoordinates a vector holding longitude and latitude of a single location on earth
 #' @return a matrix of azimuth and height
